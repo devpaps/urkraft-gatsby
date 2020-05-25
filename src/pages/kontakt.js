2b683c11abe26bd3bdec0f3a13516c38482e726c
Lars jönsson <crymeat@gmail.com>
2020-05-25 22:52:05 +0200

    contact page

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -5,7 +5,7 @@ import ContactStyle from "../components/modules/contact.module.css"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
-export default ({data}) => (
+export default () => (
   <div>
     <Layout>
       <Helmet>
@@ -13,76 +13,124 @@ export default ({data}) => (
       </Helmet>
       <section className={ContactStyle.container}>
         <h1 className={ContactStyle.headerText}>Kontakta oss</h1>
-        <form
-          name="contact"
-          method="POST"
-          netlify-honeypot="bot-field"
-          data-netlify="true"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div className={ContactStyle.contactForm}>
-            <p className={ContactStyle.hidden}>
-              <label>
-                Don’t fill this out if you're human: <input name="bot-field" />
-              </label>
-            </p>
-            <label className={ContactStyle.label}>
-              <span className={ContactStyle.span}>Ditt namn</span>
-              <input
-                type="text"
-                name="Namn"
-                className=""
-                required
-                placeholder="Johan"
-              />
-            </label>
-            <label className={ContactStyle.label}>
-              <span className={ContactStyle.span}>Email</span>
-              <input
-                type="email"
-                name="E-mail"
-                className=""
-                placeholder="namn@gmail.com"
-                required
-              />
-            </label>
-            <label className={ContactStyle.label}>
-              <span className={ContactStyle.span}>Ditt meddelande</span>
-              <textarea
-                type="text"
-                className=""
-                name="Meddelande"
-                placeholder="Vad har du på hjärtat?"
-                required
-                rows="4"
-              />
-            </label>
-            <button type="submit" className={ContactStyle.whatWeDoButton}>
-              Skicka
-            </button>
+        <div className={ContactStyle.main}>
+          <div className={ContactStyle.left}>
+            <h1>Vanliga frågor</h1>
+            <div className={ContactStyle.leftText}>
+              <p>
+                Du är viktig för oss! Vi på arbetar så fort vi kan på att svara
+                på era frågor.
+              </p>
+              <div className={ContactStyle.corona}>
+                <h2>Våra riktlinjer angående Covid-19</h2>
+                <ul>
+                  <li>Vi håller öppet som vanligt, 06-21 alla dagar.</li>
+                  <li>Träna inte om du har symptom på förkylning/influensa</li>
+                  <li>
+                    Tvätta händerna noga efteråt. Torka av maskiner och handtag
+                    med ytdesinfektionsmedel sedan du har tränat.
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-        </form>
-        {/* <Img fluid={data.staticMap.childFile.absolutePath} /> */}
-        {data.staticMap.childFile.absolutePath}
+          <div className={ContactStyle.right}>
+            <form
+              name="contact"
+              method="POST"
+              netlify-honeypot="bot-field"
+              data-netlify="true"
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <div className={ContactStyle.contactForm}>
+                <p className={ContactStyle.hidden}>
+                  <label>
+                    Don’t fill this out if you're human:{" "}
+                    <input name="bot-field" />
+                  </label>
+                </p>
+                <div className={ContactStyle.nameContainer}>
+                  <div className={ContactStyle.firstName}>
+                    <label className={ContactStyle.label} for="fornamn">
+                      Förnamn
+                    </label>
+                    <input
+                      type="text"
+                      maxlength="255"
+                      autocomplete="none"
+                      id="fornamn"
+                      name="Namn"
+                      className=""
+                      required
+                      placeholder="Förnamn"
+                    />
+                  </div>
+                  <div className={ContactStyle.lastName}>
+                    <label className={ContactStyle.label} for="efternamn">
+                      Efternamn
+                    </label>
+                    <input
+                      type="text"
+                      maxlength="255"
+                      autocomplete="none"
+                      id="efternamn"
+                      name="Namn"
+                      className=""
+                      required
+                      placeholder="Efternamn"
+                    />
+                  </div>
+                </div>
+                <label className={ContactStyle.label} for="email">
+                  E-post
+                </label>
+                <input
+                  type="email"
+                  maxlength="255"
+                  autocomplete="none"
+                  id="email"
+                  name="E-post"
+                  className=""
+                  placeholder="E-post"
+                  required
+                />
+
+                <label className={ContactStyle.label} for="meddelande">
+                  Ditt meddelande
+                </label>
+                <textarea
+                  type="text"
+                  id="meddelande"
+                  className=""
+                  name="Meddelande"
+                  placeholder="Vad har du på hjärtat?"
+                  required
+                  rows="5"
+                />
+
+                <button type="submit" className={ContactStyle.whatWeDoButton}>
+                  <div>Skicka</div>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </section>
     </Layout>
   </div>
 )
 
-
-
 export const mapQuery = graphql`
-query StaticMapQuery {
-  staticMap {
-    childFile {
-      absolutePath
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
+  query StaticMapQuery {
+    staticMap {
+      childFile {
+        absolutePath
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
         }
       }
     }
   }
-}
-
 `
